791d96a8afd9a8f61d625eb6b3e4f41bcf0360d6
saabd6875 <143174468+saabd6875@users.noreply.github.com>
2024-04-06 00:12:03 +0200

    Add files via upload

---
+++ kino/src/test/java/oslomet/webpro/KinoApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
